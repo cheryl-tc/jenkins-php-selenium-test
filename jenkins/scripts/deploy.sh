c689b14179c5ef8248cbd230246236cebc6983a0
cheryl-tc <122279995+cheryl-tc@users.noreply.github.com>
2024-07-21 21:32:41 +0800

    Fix deployment path

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,6 +3,7 @@
 set -x
 docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/chery/OneDrive/Documents/SSD/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
+docker exec my-apache-php-app chmod -R 755 /var/www/html
 set +x
 
 echo 'Now...'
